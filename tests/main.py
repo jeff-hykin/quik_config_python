dff56c29fdabdbe35906da891a31a073f5926058
Jeff Hykin <jeff.hykin@gmail.com>
2022-02-09 11:44:21 -0600

    -

--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,9 @@
 from quik_config import find_and_load
 
 # NOTE: intentionally changes directories to be in the same folder as the config
-config = find_and_load("info.yaml").config # will keep going up a directories looking for the file
+config = find_and_load("main/info.yaml").config # will keep going up a directories looking for the file
 print(f'''config.has_gpu = {config.has_gpu}''')
 print(f'''config.constants.pi = {config.constants.pi}''')
 
 # same as above but will not change your directory
-config = find_and_load("info.yaml", go_to_root=False).config
+config = find_and_load("main/info.yaml", go_to_root=False).config

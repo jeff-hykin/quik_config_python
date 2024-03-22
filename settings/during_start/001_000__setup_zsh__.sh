764af084446b3c09cadf9147bf1de27bd55832db
Jeff Hykin <jeff.hykin@gmail.com>
2024-03-22 12:27:25 -0500

    switch to lightweight custom prompt

--- settings/during_start/001_000__setup_zsh__.sh
+++ settings/during_start/001_000__setup_zsh__.sh
@@ -3,7 +3,6 @@
 # 
 zsh_syntax_highlighting__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" zsh-syntax-highlighting)"
 zsh_auto_suggest__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" zsh-autosuggestions)"
-spaceship_prompt__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" spaceship-prompt)"
 oh_my_zsh__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" oh-my-zsh)"
 zsh__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" zsh)"
 
@@ -13,22 +12,21 @@ zsh__path="$("$__FORNIX_NIX_COMMANDS/package_path_for" zsh)"
 local_zsh="$FORNIX_FOLDER/settings/.cache/zsh.do_not_sync/site-functions/"
 mkdir -p "$local_zsh"
 
-export fpath=("$local_zsh")
+export fpath=("$local_zsh" $fpath)
 export fpath=("$oh_my_zsh__path"/share/oh-my-zsh/functions $fpath)
 export fpath=("$oh_my_zsh__path"/share/oh-my-zsh/completions $fpath)
 export fpath=("$zsh__path"/share/zsh/site-functions $fpath)
-export fpath=("$zsh__path"/share/zsh/*/functions $fpath)
+export fpath=("$zsh__path"/share/zsh/site-functions $fpath)
+export fpath=("$zsh__path"/share/zsh/**/functions $fpath)
 
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell" # default
-
-# 
-# add spaceship-prompt theme
-# 
-ln -fs "$spaceship_prompt__path/lib/spaceship-prompt/spaceship.zsh" "$local_zsh"prompt_spaceship_setup
+# ZSH_THEME="robbyrussell" # default
 
 export ZSH="$oh_my_zsh__path/share/oh-my-zsh"
 . "$ZSH/oh-my-zsh.sh"
+
+autoload -Uz compinit
+compinit
 
 # 
 # enable syntax highlighing
@@ -41,15 +39,67 @@ export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR="$zsh_syntax_highlighting__path/share/zsh-
 # 
 . "$zsh_auto_suggest__path/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-SPACESHIP_CHAR_SYMBOL="∫ " # ☣ ⁂ ⌘ ∴ ∮ ֎ Ͽ ♫ ⛬ ⚿ ♦ ♢ ⛶ ✾ ❒ ⟩ ⟡ ⟜ ⟦ ⦊ ⦒ ⪢ ⪾ ∫ ∬ ∭
+# SPACESHIP_CHAR_SYMBOL="∫ " # ☣ ⁂ ⌘ ∴ ∮ ֎ Ͽ ♫ ⛬ ⚿ ♦ ♢ ⛶ ✾ ❒ ⟩ ⟡ ⟜ ⟦ ⦊ ⦒ ⪢ ⪾ ∫ ∬ ∭
 
-# Set Spaceship ZSH as a prompt
+# Custom prompt (other prompts cause problems for some reason)
 autoload -U promptinit; promptinit
-prompt spaceship
-
-# enable auto complete
-autoload -Uz compinit
-compinit
+_prompt_help__simple_pwd() {
+    local maybe_relative_to_home="${PWD/#"$HOME"/"~"}"
+    if [ -d ".git" ]
+    then
+        local path_to_file=""
+        local dir_name=".git"
+        local folder_to_look_in="$PWD"
+        while :
+        do
+            # check if file exists
+            if [ -d "$folder_to_look_in/$dir_name" ]
+            then
+                path_to_file="$folder_to_look_in"
+                break
+            else
+                if [ "$folder_to_look_in" = "/" ]
+                then
+                    break
+                else
+                    folder_to_look_in="$(dirname "$folder_to_look_in")"
+                fi
+            fi
+        done
+        if [ -z "$path_to_file" ]
+        then
+            # fallback
+            echo "$maybe_relative_to_home"
+        else
+            # git path
+            echo "$(basename "$path_to_file")"
+        fi
+        # basename "$PWD"
+    else
+        echo "$maybe_relative_to_home"
+    fi
+}
+_prompt_help__git_status() {
+    # if git exists
+    if [ -n "$(command -v "git")" ]
+    then
+        echo "  $(git rev-parse --abbrev-ref HEAD) "
+    fi
+}
+_prompt_help__python_if_available() {
+    if [ -n "$(command -v "python")" ]
+    then
+        local version_string="$(python --version)"
+        echo "Python:${version_string/#Python /}%u  "
+    fi
+}
+_prompt_help__deno_if_available() {
+    if [ -n "$(command -v "deno")" ]
+    then
+        local version_string="$(deno --version)"
+        echo "Deno:$(deno --version | sed -E 's/deno ([0-9]+\.[0-9]+\.[0-9]+).+/\1/' | head -n1)%u  "
+    fi
+}
 
 autoload bashcompinit
 bashcompinit

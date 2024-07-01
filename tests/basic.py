5a7afc5ee920d6a94823e2fea3edb7d8dc9dde4c
Jeff Hykin <jeff.hykin@gmail.com>
2024-07-01 09:24:19 -0500

    add tests

--- tests/basic.py
+++ tests/basic.py
@@ -1,7 +1,7 @@
 from quik_config import find_and_load
 
 info = find_and_load(
-    "info.yaml", # walks up folders until it finds the 
+    "tests/info.yaml", # walks up folders until it finds the 
     cd_to_filepath=True,
     parse_args=True,
     defaults_for_local_data=["PROD"], # example profiles
